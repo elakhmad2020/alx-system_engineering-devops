0c7a55ef2df893f5fa296e4b23bd58b4ac9b6d27
alakhmad2020 <ahmadabdullahibayero@gmail.com>
2024-06-06 10:54:41 -0700

    Japan

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
+                            . format(subreddit),
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
     if sub_info.status_code >= 300:
